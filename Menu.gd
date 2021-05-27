1a3756f89f91944076e75643d446bb660a6d614d
arkeve <sketchthedev@gmail.com>
2021-05-27 19:55:37 -0400

    Be able to transition to party screen

--- Menu.gd
+++ Menu.gd
@@ -1,30 +1,49 @@
 extends CanvasLayer
 
+const PokemonPartyScreen = preload("res://PokemonPartyScreen.tscn")
+
 var selected_option: int = 0
+enum ScreenLoaded { JUST_MENU, PARTY_SCREEN, SETTINGS,  }
+var screen_loaded = ScreenLoaded.JUST_MENU
+
+onready var select_arrow = $Control/NinePatchRect/SelectArrow
+onready var menu = $Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Control.visible = false
-	$Control/NinePatchRect/TextureRect.rect_position.y = 10 + (selected_option % 6) * 14
-	
+	menu.visible = false
+	select_arrow.rect_position.y = 10 + (selected_option % 6) * 14
+
+func load_party_screen():
+	menu.visible = false
+	screen_loaded = ScreenLoaded.PARTY_SCREEN
+	var party_screen = PokemonPartyScreen.instance()
+	add_child(party_screen)
+
 func _unhandled_input(event):
-	if event.is_action_pressed("menu"):
-		var player = get_parent().get_node("CurrentScene").get_children().back().find_node("Player")
-		if $Control.visible:
-			player.set_physics_process(true)
-			$Control.visible = !$Control.visible
-		else:
-			if !player.is_moving:
-				player.set_physics_process(false)
-				$Control.visible = !$Control.visible
-	
-	if $Control.visible:
-		if event.is_action_pressed("ui_up"):
-			if selected_option == 0:
-				selected_option = 5
-			else:
-				selected_option -=1
-			$Control/NinePatchRect/TextureRect.rect_position.y = 10 + (selected_option % 6) * 14
-		elif event.is_action_pressed("ui_down"):
-			selected_option +=1
-			$Control/NinePatchRect/TextureRect.rect_position.y = 10 + (selected_option % 6) * 14
+	match screen_loaded:
+		ScreenLoaded.JUST_MENU:
+			if event.is_action_pressed("menu"):
+				var player = get_parent().get_node("CurrentScene").get_children().back().find_node("Player")
+				if menu.visible:
+					player.set_physics_process(true)
+					menu.visible = !menu.visible
+				else:
+					if !player.is_moving:
+						player.set_physics_process(false)
+						menu.visible = !menu.visible
+			
+			if menu.visible:
+				if event.is_action_pressed("ui_up"):
+					if selected_option == 0:
+						selected_option = 5
+					else:
+						selected_option -=1
+					select_arrow.rect_position.y = 10 + (selected_option % 6) * 14
+				elif event.is_action_pressed("ui_down"):
+					selected_option +=1
+					select_arrow.rect_position.y = 10 + (selected_option % 6) * 14
+				elif event.is_action_pressed("z"):
+					get_parent().transition_to_party_screen()
+		ScreenLoaded.PARTY_SCREEN:
+			pass
--- SceneManager.gd
+++ SceneManager.gd
@@ -4,19 +4,37 @@ var next_scene = null
 var player_location = Vector2(0, 0)
 var player_direction = Vector2(0, 0)
 
+
+enum TransitionType {
+	NEW_SCENE, PARTY_SCREEN,
+}
+var transition_type = TransitionType.NEW_SCENE
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+	
+	
+func transition_to_party_screen():
+	$ScreenTransition/AnimationPlayer.play("FadeToBlack")
+	transition_type = TransitionType.PARTY_SCREEN
 
 func transition_to_scene(new_scene: String, spawn_location: Vector2, spawn_direction: Vector2):
 	next_scene = new_scene
 	player_location = spawn_location
 	player_direction = spawn_direction
+	transition_type = TransitionType.NEW_SCENE
 	$ScreenTransition/AnimationPlayer.play("FadeToBlack")
 	
 func finished_fading():
-	$CurrentScene.get_child(0).queue_free()
-	$CurrentScene.add_child(load(next_scene).instance())
-	var player = get_node("CurrentScene").get_children().back().find_node("Player")
-	player.set_spawn(player_location, player_direction)
-	$ScreenTransition/AnimationPlayer.play("FadeToNormal")
+	
+	match transition_type:
+		TransitionType.NEW_SCENE:
+			$CurrentScene.get_child(0).queue_free()
+			$CurrentScene.add_child(load(next_scene).instance())
+			var player = get_node("CurrentScene").get_children().back().find_node("Player")
+			player.set_spawn(player_location, player_direction)
+		TransitionType.PARTY_SCREEN:
+			$Menu.load_party_screen()
+			$ScreenTransition/AnimationPlayer.play("FadeToNormal")
