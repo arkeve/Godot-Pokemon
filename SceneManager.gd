28e8bab39c1f52a9d5a7ea73c093682db236af4a
arkeve <sketchthedev@gmail.com>
2021-05-29 10:24:23 -0400

    Add menu screen and party screen

--- SceneManager.gd
+++ SceneManager.gd
@@ -5,22 +5,40 @@ var next_scene = null
 var player_location = Vector2(0, 0)
 var player_direction = Vector2(0, 0)
 
+enum TransitionType { NEW_SCENE, PARTY_SCREEN, MENU_ONLY }
+var transition_type = TransitionType.NEW_SCENE
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
+func transition_to_party_screen():
+	$ScreenTransition/AnimationPlayer.play("FadeToBlack")
+	transition_type = TransitionType.PARTY_SCREEN
+	
+func transition_exit_party_screen():
+	$ScreenTransition/AnimationPlayer.play("FadeToBlack")
+	transition_type = TransitionType.MENU_ONLY
 
 
 func transition_to_scene(new_scene: String, spawn_location, spawn_direction):
 	next_scene = new_scene
 	player_location = spawn_location
 	player_direction = spawn_direction
+	transition_type = TransitionType.NEW_SCENE
 	$ScreenTransition/AnimationPlayer.play("FadeToBlack")
 	
 func finished_fading():
-	$CurrentScene.get_child(0).queue_free()
-	$CurrentScene.add_child(load(next_scene).instance())
-	
-	var player = $CurrentScene.get_children().back().find_node("Player")
-	player.set_spawn(player_location, player_direction)
+	match transition_type:
+		TransitionType.NEW_SCENE:
+			$CurrentScene.get_child(0).queue_free()
+			$CurrentScene.add_child(load(next_scene).instance())
+			
+			var player = $CurrentScene.get_children().back().find_node("Player")
+			player.set_spawn(player_location, player_direction)
+		TransitionType.PARTY_SCREEN:
+			$Menu.load_party_screen()
+		TransitionType.MENU_ONLY:
+			$Menu.unload_party_screen()
 	
 	$ScreenTransition/AnimationPlayer.play("FadeToNormal")
